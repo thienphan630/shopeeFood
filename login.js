a5870362d2d9cf0911c74851321c5ba5c4b5c2db
thienphan <thienphan630@gmail.com>
2021-12-15 08:57:22 +0700

    login to homepage

---
+++ login.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+function login(){
+    event.preventDefault();
+    var email = document.getElementById("staticEmail").value;
+    var password = document.getElementById("inputPassword").value;
+    fetch("./data.json")
+    .then(response => {
+        return response.json();
+    })
+    .then((data) => {
+        for(var x of data){
+            if(email==x.username && password==x.password) window.location.href="home.html";
+            else console.log('no');
+        }
+    
+    })
+    
+}
+
