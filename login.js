3294caa5c72205527bc6c670483f11d4c5df35f2
thienphan <thienphan630@gmail.com>
2021-12-16 10:13:44 +0700

    update

--- login.js
+++ login.js
@@ -1,14 +1,16 @@
 function login(){
-    var email = document.getElementById("staticEmail").value;
-    var password = document.getElementById("inputPassword").value;
+    const email = document.getElementById("staticEmail").value;
+    const password = document.getElementById("inputPassword").value;
     fetch("./data.json")
     .then(response => {
         return response.json();
     })
     .then((data) => {
-        for(var x of data){
+        for(let x of data){
             if(email==x.username && password==x.password) window.location.href="home.html";
-            else console.log('no');
+            else console.log('Bạn đã nhập sai email/password');
         }
     })
 }
+
+
