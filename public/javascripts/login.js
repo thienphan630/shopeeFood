aeb0f16ff189c09c27cb789120f03542fcce56a6
thienphan <thienphan630@gmail.com>
2021-12-24 16:56:21 +0700

    update file structure

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -2,16 +2,18 @@ function login(){
     const email = document.getElementById("staticEmail").value;
     const password = document.getElementById("inputPassword").value;
     console.log()
-    fetch("./data.json")
+    fetch("http://localhost:3000/api/data.json")
     .then(response => {
         return response.json();
     })
     .then((data) => {
-        for(let x of data){
-            if(email==x.username && password==x.password) window.location.href="home.html";
-            else console.log('Bạn đã nhập sai email/password');
-        }
+        // for(let x of data){
+        //     if(email==x.username && password==x.password) alert("ok");
+        //     else alert('Bạn đã nhập sai email/password');
+        // }
+        console.log(data);
     })
+    .catch((err)=>{alert(err);})
 }
 
 
